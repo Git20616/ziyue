75f21c4389836559277d8ad318ba8aa94968d6cf
Git20616 <961055747@qq.com>
2020-08-19 23:56:12 +0800

    2020-08-19 23:51

--- ziyue/lib/chapter3/state_manage.dart
+++ ziyue/lib/chapter3/state_manage.dart
@@ -15,12 +15,117 @@ class StateManage extends StatelessWidget {
           children: <Widget>[
             FlatButton(
               child: Text("管理自身状态"),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          TapBoxA(),
+                        ],
+                      ),
+                      color: Colors.white,
+                    );
+                  },
+                ));
+              },
+            ),
+            FlatButton(
+              child: Text("父Widget管理子Widget状态"),
               onPressed: () {},
             ),
           ],
         ),
       ),
     );
+  }
+
+}
+
+class TapBoxA extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TapBoxAState();
+  }
+
+}
+
+class TapBoxAState extends State<TapBoxA> {
+  bool _active = false;
+
+  void _handleTap () {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? "active" : "inactive",
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+
+}
+
+class ParentWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ParentWidgetState();
+  }
+
+}
+
+class ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChange(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            child: TapBoxB(),
+          ),
+        ],
+      ),
+      color: Colors.white,
+    );;
+  }
+
+}
+
+class TapBoxB extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 
 }
